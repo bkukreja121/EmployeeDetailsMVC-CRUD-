3e2de5e1ff13c90f489a5ec5c8d517a6d3e2ab02
Bhaveshk <Bhavishkukreja16@gmail.com>
2021-09-14 08:55:57 +0530

    the  final commit

---
+++ vs/RepositoryLayer/Interface/IEmployeeRL.cs
@@ -0,0 +1,16 @@
+﻿using CommonLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IEmployeeRL
+    {
+        List<Employee> EmployeeList();
+
+        bool InsertEmployee(Parent employee);
+
+        bool DeleteEmployee(Parent employee);
+    }
+}
